ef96239e3a5499758b37baedfba49d1625ccb4e7
Raj9408 <149301957+Raj9408@users.noreply.github.com>
2024-06-24 23:04:53 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,44 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {};
 
-// export default nextConfig;
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(mp4)$/,
-            use: 'file-loader',
-        });
-        return config;
-    },
+  images: {
+    disableStaticImages: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|ico|webp|jp2|avif|tiff?)$/,
+      include: path.resolve(__dirname, 'src/assets/images'),
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/images/[name].[hash][ext]',
+      },
+    });
+
+    return config;
+  },
 };
 
-export default nextConfig;
+export default nextConfig;
+
+// export default nextConfig;
+// const nextConfig = {
+//     webpack: (config) => {
+//         config.module.rules.push({
+//             test: /\.(mp4)$/,
+//             use: 'file-loader',
+//         });
+//         return config;
+//     },
+// };
+
+// export default nextConfig;
 
 // const nextConfig = {
 //     webpack: (config, { isServer }) => {
